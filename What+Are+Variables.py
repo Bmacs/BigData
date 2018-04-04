b7b4f07160cfb2ee3a04ff21844e8c5940f989aa
Brendan McNamara <brendancmcnamara@gmail.com>
2018-04-04 01:02:39 -0400

    Initial commit

---
+++ What+Are+Variables.py
@@ -0,0 +1,88 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print("Hello World")
+
+
+# In[2]:
+
+print('more string')
+
+
+# In[3]:
+
+print(3)
+
+
+# In[4]:
+
+print("New England Tech")
+print(7)
+
+
+# In[5]:
+
+a = 1
+
+
+# In[6]:
+
+print(a)
+
+
+# In[7]:
+
+b = 1
+
+
+# In[8]:
+
+c = "hello there"
+print(c)
+
+
+# In[9]:
+
+c = 1
+
+
+# In[10]:
+
+print(c)
+
+
+# In[11]:
+
+print(e)
+
+
+# In[12]:
+
+f = a
+print(f)
+
+
+# In[13]:
+
+a = 2
+print(a)
+print(f)
+
+
+# In[14]:
+
+v1 = "first string"
+v2 = "second string"
+v3 = v1
+v1 = v2
+v2 = v3
+print(v1)
+print(v2)
+
+
+# In[ ]:
+
+
+
